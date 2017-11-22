ab7541ca52da15436bd7b1af5c88a3ce129d7f4b
joachimprinzbach <joachim.prinzbach@gmail.com>
2017-11-22 23:05:39 +0100

    Add some components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {MenuComponent} from './menu/menu.component';
+import {SecondComponent} from './second/second.component';
 
 
-import { AppComponent } from './app.component';
-
+const appRoutes: Routes = [
+    {path: 'second', component: SecondComponent},
+    {path: 'home', component: HomeComponent},
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {path: '**', component: HomeComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MenuComponent,
+        HomeComponent,
+        SecondComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
